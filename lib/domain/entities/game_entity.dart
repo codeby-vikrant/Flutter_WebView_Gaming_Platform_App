4b98624ae2c0a9be186b05037fce507fdeb3308b
Vikrant Vani <workwithvikrant0111@gmail.com>
2024-10-28 13:16:55 +0530

    added required entities like- 1. Game name 2. Game url 3. Game logo

---
+++ lib/domain/entities/game_entity.dart
@@ -0,0 +1,11 @@
+class GameEntity{
+  final String name;
+  final String url;
+  final String logoUrl;
+
+  GameEntity({
+    required this.name,
+    required this.url,
+    required this.logoUrl
+});
+}
