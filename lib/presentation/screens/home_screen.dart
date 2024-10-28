b700bf93d618c30b9afbe197a5cc09109fd3b9b7
Vikrant Vani <workwithvikrant0111@gmail.com>
2024-10-28 14:14:53 +0530

    reformating the homepage code

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:platform_web_view/presentation/providers/game_provider.dart';
+import 'package:platform_web_view/presentation/screens/web_view.dart';
 import 'package:provider/provider.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
@@ -16,28 +17,30 @@ class HomeScreen extends StatelessWidget {
         title: Text('Game Platforms'),
       ),
       body: ListView.builder(
-        itemCount: gameProvider.games.length,
-        itemBuilder: (context, index){
-          final game = gameProvider.games[index];
-          return ListTile(
-            leading: CachedNetworkImage(imageUrl: game.logoUrl, placeholder: (context, url) => const CircularProgressIndicator(),
-            errorWidget: (context, url, error) => const Icon(Icons.error),
-              width: 50,
-              height: 50,
-              fit: BoxFit.cover,
-            ),
-            title: Text(game.name),
-            onTap: (){
-              Navigator.push(
-               context,
-                MaterialPageRoute(
-                  builder: (context) => WebViewScreen(url: game.url),
-                ),
-              );
-            },
-          );
-        }
-      ),
+          itemCount: gameProvider.games.length,
+          itemBuilder: (context, index) {
+            final game = gameProvider.games[index];
+            return ListTile(
+              leading: CachedNetworkImage(
+                imageUrl: game.logoUrl,
+                placeholder: (context, url) =>
+                    const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+                width: 50,
+                height: 50,
+                fit: BoxFit.cover,
+              ),
+              title: Text(game.name),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => WebViewScreen(url: game.url),
+                  ),
+                );
+              },
+            );
+          }),
     );
   }
 }
