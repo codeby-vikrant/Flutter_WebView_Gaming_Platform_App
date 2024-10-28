e586ffaa933dd64f70a5aa278d025630b8e0fc0e
Vikrant Vani <workwithvikrant0111@gmail.com>
2024-10-28 13:57:41 +0530

    used provider to fetch games added a scaffold with an app bar and list to display games

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:platform_web_view/presentation/providers/game_provider.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final gameProvider = Provider.of<GameProvider>(context);
+    gameProvider.fetchGames();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Game Platforms'),
+      ),
+      body: ListView.builder(
+        itemCount: gameProvider.games.length,
+        itemBuilder: (context, index){
+
+        }
+      ),
+    );
   }
 }
