20a509976f133b91279cb51b2f48d99d6350337a
Vikrant Vani <workwithvikrant0111@gmail.com>
2024-10-28 14:16:34 +0530

    created stateful widget and adding url required for web view class

---
+++ lib/presentation/screens/web_view.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class WebViewScreen extends StatefulWidget {
+  final String url;
+  const WebViewScreen({super.key, required this.url});
+
+  @override
+  State<WebViewScreen> createState() => _WebViewScreenState();
+}
+
+class _WebViewScreenState extends State<WebViewScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
